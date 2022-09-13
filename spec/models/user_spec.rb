73ed1373cd04e8193b2b6c90cf61743f9082a7cb
Caitlin <cjcroteau@gmail.com>
2022-09-13 23:34:36 +0000

    add user spec and validation tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validation' do
+    it 'should save user when all required fields are set correctly' do
+      @user = User.new(name: 'Caitlin C.', email: 'lol@aol.com', password: "12345hello", password_confirmation: "12345hello")
+      expect(@user).to be_valid
+    end
+
+    it 'should not save if password and password_confirmation do not match' do
+      @user = User.new(name: 'Caitlin C.', email: 'lol@aol.com', password: "12345hello", password_confirmation: "6789hello")
+      @user.save 
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'should provide an error message if password not set' do
+      @user = User.new(name: 'Caitlin C.', email: 'lol@aol.com', password: nil, password_confirmation: nil)
+      @user.save
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'should provide an error message if name not set' do
+      @user = User.new(name: nil, email: 'lol@aol.com', password: "12345hello", password_confirmation: "12345hello")
+      @user.save
+      # expect(@user).to_not be_valid
+      # puts @user.errors.full_messages
+      expect(@user.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'should provide an error message if email not set' do
+      @user = User.new(name: 'Caitlin C.', email: nil, password: "12345hello", password_confirmation: "12345hello")
+      @user.save
+      # expect(@user).to_not be_valid
+      # puts @user.errors.full_messages
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    xit 'should provide an error message if email is not unique' do
+      puts "look into how to test this."
+    end
+
+  end
+
+end
+
+
+# it 'should have a minimun password length' do
+#   @user = User.new(name: 'Caitlin C.', email: 'lol@aol.com', password: "We", password_confirmation: "We")
+#   @user.save
+#   expect(@user).to_not be_valid
+#   puts @user.errors.full_messages
+# end
