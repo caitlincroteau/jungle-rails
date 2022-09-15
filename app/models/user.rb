495245eb78e378fa7652f43117c84e749c650f8b
Caitlin <cjcroteau@gmail.com>
2022-09-15 20:06:06 +0000

    refactor user authentication to check credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :password, length: { minimum: 5 }
+
+  def authenticate_with_credentials(email, password)
+
+    @user = User.find_by_email(email)
+
+    if @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+
+  end
+
 end
